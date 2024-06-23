884f9ef2ad38ce8d152f8c9d98275f82d6134022
Sujith Varughese <sujithv15@gmail.com>
2024-06-23 00:31:31 -0400

    dashboard page with outlet from app-bar

--- src/components/nav/AppBar.jsx
+++ src/components/nav/AppBar.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
+import { Outlet } from "react-router-dom"
 import { styled } from '@mui/material/styles'
 import MuiAppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu.js'
+import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography'
 import Badge from '@mui/material/Badge'
 import NotificationsIcon from '@mui/icons-material/Notifications.js'
 import Button from '@mui/material/Button'
 import MenuItem from '@mui/material/MenuItem'
 import { useAuthProvider } from '../../context/auth-context.jsx'
+import MuiDrawer from '@mui/material/Drawer'
+import Box from '@mui/material/Box'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft.js'
+import Divider from '@mui/material/Divider'
+import List from '@mui/material/List'
+import { mainListItems, secondaryListItems } from './listItems.jsx'
 
 const drawerWidth = 240;
 
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
 const StyledAppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
@@ -40,46 +73,85 @@ const AppBar = () => {
   };
 
   return (
-    <StyledAppBar position="absolute" open={open}>
-      <Toolbar
-        sx={{
-          pr: '24px', // keep right padding when drawer closed
-        }}
-      >
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={toggleDrawer}
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <StyledAppBar position="absolute" open={open}>
+        <Toolbar
           sx={{
-            marginRight: '36px',
-            ...(open && { display: 'none' }),
+            pr: '24px', // keep right padding when drawer closed
           }}
         >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ flexGrow: 1 }}
-        >
-          Dashboard
-        </Typography>
-        <IconButton color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
 
-        <MenuItem>
-          <Button color="secondary" variant="contained" sx={{ width: '100%' }} onClick={signOutUser}>
-            Log Out
-          </Button>
-        </MenuItem>
-      </Toolbar>
-    </StyledAppBar>
+          <MenuItem>
+            <Button color="secondary" variant="contained" sx={{ width: '100%' }} onClick={signOutUser}>
+              Log Out
+            </Button>
+          </MenuItem>
+        </Toolbar>
+      </StyledAppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          {mainListItems}
+          <Divider sx={{ my: 1 }} />
+          {secondaryListItems}
+        </List>
+      </Drawer>
+
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+
   )
 }
 
