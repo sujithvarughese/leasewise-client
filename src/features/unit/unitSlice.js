0d94db04c8c86011acffb653db6a8aa730c7c87d
Sujith Varughese <sujithv15@gmail.com>
2024-06-25 21:40:20 -0400

    redux

---
+++ src/features/unit/unitSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  units: []
+}
+
+const unitSlice = createSlice({
+  name: "unit",
+  initialState,
+  reducers: {
+
+  }
+})
+
+export default unitSlice.reducer
