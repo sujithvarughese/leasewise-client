80076953830cd1b23831622caea0037f57d4d1c7
Sujith Varughese <sujithv15@gmail.com>
2024-07-01 11:50:45 -0400

    tabs for unit page

--- src/components/forms/CreateExpenseForm.jsx
+++ src/components/forms/CreateExpenseForm.jsx
@@ -8,6 +8,8 @@ import CustomInput from '../ui/CustomInput.jsx'
 import React from 'react'
 import Button from '@mui/material/Button'
 import FormModal from '../ui/FormModal.jsx'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 
 const CreateExpenseForm = ({ open, onClose }) => {
 
@@ -31,17 +33,26 @@ const CreateExpenseForm = ({ open, onClose }) => {
 
     <FormModal open={open} onClose={onClose} heading="Create Expense">
       <form onSubmit={handleSubmit}>
-        <StyledSelect name="category" label="Category" options={categories} minWidth={120}/>
-        <TextField id="payTo" name="payTo" label="Pay To" variant="outlined" />
-        <TextField type="number" id="amount" name="amount" label="Amount" variant="outlined" />
-        <TextField type="number" id="balance" name="balance" label="Balance" variant="outlined" />
-        <TextField type="date" id="dateDue" name="dateDue" label="Due Date" variant="outlined" />
-        <TextField type="date" id="datePaid" name="datePaid" label="Date Paid" variant="outlined" />
-        <TextField id="companyName" name="companyName" label="Company" variant="outlined" />
-        <TextField id="companyAddress" name="companyAddress" label="Company Address" variant="outlined" />
-        <TextField id="companyPhone" name="companyPhone" label="Company Phone" variant="outlined" />
-        <TextField type="email" id="companyEmail" name="companyEmail" label="Company Email" variant="outlined" />
-        <Button type="submit">Submit</Button>
+        <Stack gap={2}>
+          <StyledSelect name="category" label="Category" options={categories} minWidth={120}/>
+          <TextField id="payTo" name="payTo" label="Pay To" variant="outlined" />
+          <Stack flexDirection="row" gap={2}>
+            <TextField type="number" id="amount" name="amount" label="Amount" variant="outlined" />
+            <TextField type="number" id="balance" name="balance" label="Balance" variant="outlined" />
+          </Stack>
+          <Stack flexDirection="row" gap={2}>
+            <TextField type="date" id="dateDue" name="dateDue" helperText="Due Date" variant="outlined" />
+            <TextField type="date" id="datePaid" name="datePaid" helperText="Date Paid" variant="outlined" />
+          </Stack>
+          <TextField id="companyName" name="companyName" label="Company" variant="outlined" />
+          <TextField id="companyAddress" name="companyAddress" label="Company Address" variant="outlined" />
+          <Stack flexDirection="row" gap={2}>
+            <TextField id="companyPhone" name="companyPhone" label="Company Phone" variant="outlined" />
+            <TextField type="email" id="companyEmail" name="companyEmail" label="Company Email" variant="outlined" />
+          </Stack>
+          <Button type="submit">Submit</Button>
+        </Stack>
+
       </form>
     </FormModal>
 
---
+++ src/components/units/TabMortgage.jsx
@@ -0,0 +1,11 @@
+import TabPanel from '@mui/lab/TabPanel'
+
+const TabMortgage = ({ unitMortgage }) => {
+  return (
+    <TabPanel value="mortgage">
+      Mortgage
+    </TabPanel>
+  )
+}
+
+export default TabMortgage
